a0f293a0def262ffdb2aa687f414f28845cfab61
theaskou <thsk0001@stud.kea.dk>
2023-11-13 13:24:51 +0100

    Main added

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        cat.makeSound();
+
+        dog.makeSound();
+
+    }
+}

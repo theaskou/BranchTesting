92603ee910c0473004fd65c3ba28cda355e00c53
theaskou <thsk0001@stud.kea.dk>
2023-11-13 14:07:47 +0100

    rettet main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
         Animal dog = new Dog();
         Animal cat = new Cat();
-
         cat.makeSound();
-
         dog.makeSound();
-
-
         cat.eatFood("fish");
-
         dog.eatFood("meat");
-
     }
 }
